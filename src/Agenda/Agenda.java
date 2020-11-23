101a11ee50c14c7cd2ed2b41dd3a5e0eb140cfff
Oscar <oscarmagallon2002@hotmail.com>
2020-11-23 20:44:20 +0100

    ahorcado Java casi

--- src/Agenda/Agenda.java
+++ src/Agenda/Agenda.java
@@ -3,27 +3,52 @@ package Agenda;
 import java.util.Scanner;
 
 public class Agenda {
-	public static void editarContacto (String mDatos[][]) {
-		Scanner leer=new Scanner(System.in);
-		String edit="";
-		System.out.println("Â¿Que contacto quieres editar?");
-		edit=leer.next();
-	}
-	
-	public static void borrar (String mDatos[][]) {
-		Scanner leer=new Scanner(System.in);
-		String borrar="";
-		System.out.println("Dime el nombre del contacto a borrar");
-		borrar=leer.next();
-		for (int i = 0; i<mDatos.length;i++) {
-			if (mDatos[i][0].equalsIgnoreCase(borrar)||mDatos[i][1].equals(borrar)) {
-				mDatos[i][0]="";
-				mDatos[i][1]="";
-				
+
+	public static void mostrarTodo(String mDatos[][]) {
+		for (int i = 0; i < mDatos.length; i++) {
+			if (!mDatos[i][0].equalsIgnoreCase("") || !mDatos[i][1].equalsIgnoreCase("")) {
+				System.out.println("El nombre del contacto " + i + 1 + " es " + mDatos[i][0] + " y su telefono es "
+						+ mDatos[i][1]);
 			}
 		}
 	}
-	
+
+	public static void editarContacto(String mDatos[][]) {
+		Scanner leer = new Scanner(System.in);
+		String edit = "", editnom = "", editel = "";
+		System.out.println("¿Que contacto quieres editar?");
+		edit = leer.next();
+		for (int i = 0; i < mDatos.length; i++) {
+			if (mDatos[i][0].equalsIgnoreCase(edit) || mDatos[i][1].equalsIgnoreCase(edit)) {
+				System.out.println("¿Que nombre quires ponerle?");
+				editnom = leer.next();
+				mDatos[i][0] = editnom;
+				System.out.println("Cual es su telefono");
+				editel = leer.next();
+				mDatos[i][1] = editel;
+				break;
+
+			} else {
+				System.out.println("El contacto no está");
+				break;
+			}
+		}
+	}
+
+	public static void borrar(String mDatos[][]) {
+		Scanner leer = new Scanner(System.in);
+		String borrar = "";
+		System.out.println("Dime el nombre del contacto a borrar");
+		borrar = leer.next();
+		for (int i = 0; i < mDatos.length; i++) {
+			if (mDatos[i][0].equalsIgnoreCase(borrar) || mDatos[i][1].equals(borrar)) {
+				mDatos[i][0] = "";
+				mDatos[i][1] = "";
+
+			}
+		}
+	}
+
 	public static void llenarMatriz(String mDatos[][]) {
 		for (int i = 0; i < mDatos.length; i++) {
 			for (int j = 0; j < mDatos[i].length; j++) {
@@ -32,44 +57,45 @@ public class Agenda {
 		}
 
 	}
-	public static void buscar (String mDatos[][],String nombusc) {
-		Scanner leer=new Scanner(System.in);
+
+	public static void buscar(String mDatos[][], String nombusc) {
+		Scanner leer = new Scanner(System.in);
 		System.out.println("Dime el nombre a buscar");
-		nombusc=leer.next();
-		for (int i = 0;i<mDatos.length;i++) {
-			if (mDatos[i][0].equalsIgnoreCase(nombusc)||mDatos[i][1].equalsIgnoreCase(nombusc)) {
-				System.out.println("El nombre es "+mDatos[i][0] + " su numero de telefono es "+ mDatos[i][1]);
+		nombusc = leer.next();
+		for (int i = 0; i < mDatos.length; i++) {
+			if (mDatos[i][0].equalsIgnoreCase(nombusc) || mDatos[i][1].equalsIgnoreCase(nombusc)) {
+				System.out.println("El nombre es " + mDatos[i][0] + " su numero de telefono es " + mDatos[i][1]);
 			}
 		}
 	}
 
-	public static void guardarContactos (String mDatos[][]) {
+	public static void guardarContactos(String mDatos[][]) {
 		Scanner leer = new Scanner(System.in);
-		for (int i = 0; i<mDatos.length;i++) {
+		for (int i = 0; i < mDatos.length; i++) {
 			if (mDatos[i][0].equalsIgnoreCase("")) {
 				System.out.println("Dime el nombre del contacto");
-				mDatos[i][0]= leer.next();
+				mDatos[i][0] = leer.next();
 				System.out.println("Dime el numero del contacto");
-				mDatos[i][1]=leer.next();
+				mDatos[i][1] = leer.next();
 				break;
 			}
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int filas = 0, columnas = 2, num = 0;
-		String mDatos[][],nombusc="";
+		String mDatos[][], nombusc = "";
 		Scanner leer = new Scanner(System.in);
-		System.out.println("Dime el tamaÃ±o de las filas");
+		System.out.println("Dime el tamano de las filas");
 		filas = leer.nextInt();
 		mDatos = new String[filas][2];
 		llenarMatriz(mDatos);
 
 		while (num != 6) {
-			System.out.println("1-AÃ±adir nuevo contacto");
+			System.out.println("1-Anadir nuevo contacto");
 			System.out.println("2-Buscar contacto");
 			System.out.println("3-Borrar contacto");
 			System.out.println("4-Editar los contactos");
@@ -88,15 +114,14 @@ public class Agenda {
 				borrar(mDatos);
 				break;
 			case 4:
-				
-				
-				
+				editarContacto(mDatos);
+			case 5:
+				mostrarTodo(mDatos);
 
-
-			
 			}
 
 		}
+		System.out.println("El programa ha terminado");
 
 	}
 
